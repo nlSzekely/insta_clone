3a3991aa0ca36b6ce0f4bc75dcf3d4e27aac1623
Szekely Laszlo <nlszekely@gmail.com>
2021-09-21 22:40:47 +0300

    Camera- taking pictures

--- screens/main/Add.js
+++ screens/main/Add.js
@@ -1,10 +1,85 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Image, Button} from 'react-native';
+import {Camera} from 'expo-camera';
 
-export default function Add() {
-    return (
-        <View>
-            <Text>Add</Text>
-        </View>
-    )
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  useEffect(() => {
+    console.log(camera);
+  }, [camera]);
+
+  async function takePicture() {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      setImage(data.uri);
+    }
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <>
+      <View style={styles.cameraContainer}>
+        <Camera
+          style={styles.camera}
+          type={type}
+          ref={(ref) => setCamera(ref)}
+        />
+      </View>
+      {image && <Image source={{uri:image}}  style={{flex:1}}/>}
+      <Button
+        title='Flip Image'
+        style={styles.button}
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      />
+      <Button title='Take picture' onPress={takePicture} />
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  camera: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
+  },
+});
